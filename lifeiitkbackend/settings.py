c65110a811615be8f6ce93b8fa47c17907ac508a
Rohit Ranjan <munnuranjanspiker@gmail.com>
2019-06-08 23:05:31 +0530

    create many-to-many Relation b/w events and tags

--- lifeiitkbackend/settings.py
+++ lifeiitkbackend/settings.py
@@ -43,16 +43,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'events.apps.EventsConfig'
-    'tags.apps.TagsConfig'
-    'users.apps.UsersConfig'
+    'events.apps.EventsConfig',
+    'tags.apps.TagsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
     'django_extensions',
 ]
 
