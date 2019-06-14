afb180918be9b849ea17087af3c62ba8ad2fd493
goyal-apoorv <apoorvg@iitk.ac.in>
2019-06-15 02:13:01 +0530

    Added API to fetch event by month

--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView, ListAPIView
 
 from .models import EventModel
 from .Serializer import EventSerializer
@@ -7,3 +7,11 @@ class SingleEventView(RetrieveAPIView):
     queryset = EventModel.objects.all()
     serializer_class = EventSerializer
 
+class MonthEventView(ListAPIView):
+    serializer_class = EventSerializer
+    
+    def get_queryset(self):
+        query_month = self.request.GET.get("month")
+        if(query_month is not None):
+            return EventModel.objects.filter(date__month=query_month).order_by("date", "time")
+        return EventModel.objects.all()
