f73e9663aa39c08d7a2b3a61a95dfe8a734014d0
dipeshdk <dipeshmps@gmail.com>
2019-06-25 04:52:45 +0530

    Created create events API

--- tags/models.py
+++ tags/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class TagModel(models.Model):
     tag_id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length = 40)
-    description = models.CharField(max_length = 100,blank=True,null=True)
+    description = models.CharField(max_length = 100,blank=True, default="")
