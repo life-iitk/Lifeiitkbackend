0db73e16d70f354d9351ab835ab3b5654fe7ae41
Nikhil Agarwal <nikhilag@iitk.ac.in>
2019-06-27 02:13:24 +0530

    Fetch subscribed events by month

--- events/views.py
+++ events/views.py
@@ -16,16 +16,18 @@ class MonthEventView(ListAPIView):
     serializer_class = EventSerializer
     
     def get_queryset(self):
-        query_month = self.request.data.get("month")
-        if (query_month is not None):
-            return EventModel.objects.filter(date__month=query_month).order_by("date", "time")
+        query_month = self.request.GET.get("month")
+        query_year = today.year
+        query_year = self.request.GET.get("year")
+        if (query_month is not None and query_year is not None):
+            return EventModel.objects.filter(date__year=query_year, date__month=query_month).order_by("date", "time")
         return EventModel.objects.all()
 
 class VenueEventView(ListAPIView):
     serializer_class = EventSerializer
 
     def get_queryset(self):
-        query_venue = self.request.data.get("venue")
+        query_venue = self.request.GET.get("venue")
         if (query_venue is not None):
             return EventModel.objects.filter(date__year=today.year, date__month=today.month, date__day=today.day, venue=query_venue).order_by("time")
         return EventModel.objects.all()
@@ -46,5 +48,24 @@ class FeedEventView(ListAPIView):
                 for tags in event_tags:
                     if tags.tag_id in tag_ids:
                         event_ids.append(objects.event_id)
-            return EventModel.objects.filter(event_id__in=event_ids)
+            return EventModel.objects.filter(event_id__in=event_ids).order_by("date__year", "date__month", "date__day", "time")
+        return None
+
+class Feed_MonthEventView(ListAPIView):
+    serializer_class = EventSerializer
+
+    def get_queryset(self):
+        user = IsLoggedIn(self.request)
+        if user is not None:
+            print(EventModel.objects.all()[0].tags, EventModel.objects.all()[1].tags)
+            tags = user.tags.all()
+            tag_ids = [o.tag_id for o in tags]
+            events = EventModel.objects.all()
+            event_ids = list()
+            for objects in events:
+                event_tags = objects.tags.all()
+                for tags in event_tags:
+                    if tags.tag_id in tag_ids:
+                        event_ids.append(objects.event_id)
+            return EventModel.objects.filter(event_id__in=event_ids, date__year=today.year, date__month=today.month).order_by("date", "time")
         return None
